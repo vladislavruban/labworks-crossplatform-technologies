6997e8a639686149ddeff6eff636f0eac7aeb6ae
Vladislav <v.ruban@student.csn.khai.edu>
2020-10-04 14:19:52 +0300

    Console application

---
+++ src/crossplatform/tech/Main.java
@@ -0,0 +1,19 @@
+package crossplatform.tech;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Group: 545a\n" +
+                "Student: Ruban Vladislav Valerievich\n" +
+                "Variant: 10\n" +
+                "xxxxxxxx   xx     xx  xx     xx \n" +
+                "xx    xx   xx     xx  xx     xx \n" +
+                "xx    xx   xx     xx  xx     xx \n" +
+                "xxxxxxxx   xx     xx  xx     xx \n" +
+                "xxxx       xx     xx  xx     xx \n" +
+                "xx xx      xx     xx  xx     xx \n" +
+                "xx  xx      xx   xx    xx   xx  \n" +
+                "xx   xx      xx xx      xx xx   \n" +
+                "xx    xx      xxx        xxx    \n");
+    }
+}
